9d7188bfd61fb254a48faf542c8874cad0ca0428
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-05-07 23:24:22 -0700

    created new base files and changed some tables and routes around

--- README.md
+++
@@ -1,2 +0,0 @@
-# The-Bloggers-Den
-https://www.youtube.com/watch?v=2sXjmewEQOY
--- models/index.js
+++ models/index.js
@@ -1,23 +1,13 @@
 const User = require('./User');
-const Project = require('./Post');
+const Project = require('./Project');
 
-User.hasMany(Posts, {
-foreignKey: 'user_id',
-onDelete: 'CASCADE'
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
-
-User.hasMany(Comments, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
 });
 
-Posts.belongsTo(User, {
-foreignKey: 'user_id'
-});
-
-Comments.belongsTo(User, {
-    foreignKey: 'user_id'
-});
-
-module.exports = { User, Posts, Comments };
+module.exports = { User, Project };
