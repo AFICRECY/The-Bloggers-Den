41ed6819a7385a831301e90f4fb65eee0f3a6d8d
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-05-08 20:15:54 -0700

    changing the names of classes and ids to align with theme

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   console.log(req.body)
 
   try {
-    const newProject = await Project.create({
+    const newProject = await Post.create({
       ...req.body,
       user_id: req.session.user_id,
     });
@@ -19,7 +19,7 @@ router.post('/', withAuth, async (req, res) => {
 
 router.post('/:id',withAuth, async (req, res) => {
   try {
-    const projectData = await Project.update({...req.body}, {
+    const projectData = await Post.update({...req.body}, {
       where: {
         id: req.params.id
       }
@@ -38,7 +38,7 @@ router.post('/:id',withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const projectData = await Post.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
