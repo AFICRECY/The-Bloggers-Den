ff360548eb7b8f96d2ef92f906a9911d25526a3d
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-05-08 18:00:12 -0700

    worked on client side js:updated the conditional statement to only include title and content as wellas created an updateHandler function and added fetch request to grab the user input and update the user profile with the updated post.

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,13 +2,12 @@ const newFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
   const description = document.querySelector('#project-desc').value.trim();
 
-  if (name && needed_funding && description) {
+  if (title && content) {
     const response = await fetch(`/api/projects`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ title, content }),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -38,10 +37,35 @@ const delButtonHandler = async (event) => {
   }
 };
 
+const updateButtonHandler = async (event) => {
+  const title = document.querySelector("#project-title").value.trim();
+  const content = document.querySelector("#project-content").value.trim();
+  const comment = document.querySelector("#project-comment").value.trim();
+  
+
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+    const response = await fetch(`/api/projects/${id}`, {
+      method: "POST",
+      body: JSON.stringify({ title, content ,comment}),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Failed to update project");
+    }
+  }
+};
+
+const addCommentField = (event) => {
+  const commentBox = document.querySelector(".comment-box");
+  commentBox.classList.remove("comment-box");
+};
+
 document
   .querySelector('.new-project-form')
   .addEventListener('submit', newFormHandler);
-
-document
-  .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
