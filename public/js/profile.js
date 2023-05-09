5e49729cdb4e210b079ebe5ee3fbeeace7cc7027
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-05-08 20:06:21 -0700

    Fixed some bugs and errors

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,38 +1,38 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const title = document.querySelector("#project-name").value.trim();
+  const content = document.querySelector("#project-desc").value.trim();
 
   if (title && content) {
     const response = await fetch(`/api/projects`, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({ title, content }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace("/profile");
     } else {
-      alert('Failed to create project');
+      alert("Failed to create project");
     }
   }
 };
 
 const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
 
     const response = await fetch(`/api/projects/${id}`, {
-      method: 'DELETE',
+      method: "DELETE",
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace("/profile");
     } else {
-      alert('Failed to delete project');
+      alert("Failed to delete project");
     }
   }
 };
@@ -41,7 +41,7 @@ const updateButtonHandler = async (event) => {
   const title = document.querySelector("#project-title").value.trim();
   const content = document.querySelector("#project-content").value.trim();
   const comment = document.querySelector("#project-comment").value.trim();
-
+  
 
   if (event.target.hasAttribute("data-id")) {
     const id = event.target.getAttribute("data-id");
@@ -67,5 +67,13 @@ const addCommentField = (event) => {
 };
 
 document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
+  .querySelector(".new-project-form")
+  .addEventListener("submit", newFormHandler);
+
+// document
+//   .querySelector("#delete-project")
+//   .addEventListener("click", delButtonHandler);
+
+// document
+//   .querySelector("#update-project")
+//   .addEventListener("click", updateButtonHandler);
